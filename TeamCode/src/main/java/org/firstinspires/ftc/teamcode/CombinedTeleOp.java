2898c40608e25660dcde6966b0cc1b0550731fbf
FlashDragonCoderX-KB <balasubramaniamkrithik@gmail.com>
2024-11-04 19:52:00 -0600

    Update CombinedTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedTeleOp.java
@@ -1,126 +1,133 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.teamcode;
+
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+/*
+ * This OpMode scans a single servo back and forward until Stop is pressed.
+ * The code is structured as a LinearOpMode
+ * INCREMENT sets how much to increase/decrease the servo position each cycle
+ * CYCLE_MS sets the update period.
+ *
+ * This code assumes a Servo configured with the name "left_hand" as is found on a Robot.
+ *
+ * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
+ * connected servos are able to move freely before running this test.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+@TeleOp(name = "Concept: Scan Servo", group = "TeleOp")
 
-@TeleOp(name="CombinedTeleOpWithArmCorrection", group="Linear Opmode")
-public class CombinedTeleOp extends LinearOpMode {
-    // Declare motors and servos
-    DcMotor motorFrontLeft;
-    DcMotor motorFrontRight;
-    DcMotor motorBackLeft;
-    DcMotor motorBackRight;
-    DcMotor armMotor;
-    Servo intake; // Intaking servo
-    Servo wrist; // Wrist servo
+public class ConceptScanServo extends LinearOpMode {
 
-    // Arm control variables
-    final double ARM_TICKS_PER_DEGREE = 7125.16 / 360;
-    final double ARM_COLLAPSED_INTO_ROBOT = 0;
-    final double ARM_COLLECT = 250 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SAMPLE_IN_LOW = 160 * ARM_TICKS_PER_DEGREE;
-    final double ARM_CLEAR_BARRIER = 230 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SPECIMEN = 160 * ARM_TICKS_PER_DEGREE;
-    final double ARM_ATTACH_HANGING_HOOK = 120 * ARM_TICKS_PER_DEGREE;
-    final double ARM_WINCH_ROBOT = 15 * ARM_TICKS_PER_DEGREE;
+    static final double INCREMENT   = 0.1;     // 0.01 amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS    =  500;     // period of each cycle
+    static final double MAX_POS     =  1;     // Maximum rotational position
+    static final double MIN_POS     =  0.0;     // Minimum rotational position
 
-    final double INTAKE_COLLECT = -1.0;
-    final double INTAKE_OFF = 0.0;
-    final double INTAKE_DEPOSIT = 0.5;
+    // Define class members
+    Servo   servo[];
+    double  position = (MAX_POS);// - MIN_POS) / 2; // Start at halfway position
 
-    final double WRIST_FOLDED_IN = 0.8333;
-    final double WRIST_FOLDED_OUT = 0.5;
 
-    double armPosition = ARM_COLLAPSED_INTO_ROBOT;
-    double armPositionFudgeFactor;
-    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
 
+    boolean rampUp = true;
+
+
+    static final int SERVO_COUNT = 2;
     @Override
     public void runOpMode() {
-        // Initialize motors and servos
-        motorFrontLeft = hardwareMap.get(DcMotor.class, "Left_front");
-        motorFrontRight = hardwareMap.get(DcMotor.class, "Right_front");
-        motorBackLeft = hardwareMap.get(DcMotor.class, "Left_rear");
-        motorBackRight = hardwareMap.get(DcMotor.class, "Right_rear");
-        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        intake = hardwareMap.get(Servo.class, "intake");
-        wrist = hardwareMap.get(Servo.class, "wrist");
 
-        // Set motor directions
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        servo  = new  Servo [SERVO_COUNT];
+        // Connect to servo (Assume Robot Left Hand)
+        // Change the text in quotes to match any servo name on your robot.
+        servo[0] = hardwareMap.get(Servo.class, "intake");
+        servo[1] = hardwareMap.get(Servo.class, "wrist");
+//        servo[2] = hardwareMap.get(Servo.class, "servo3");
+//        servo[3] = hardwareMap.get(Servo.class, "servo4");
 
-        // Set arm motor to use encoders
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // Initialize wrist in folded position
-        wrist.setPosition(WRIST_FOLDED_IN);
-
-        // Wait for the game driver to press play
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to scan Servo." );
+        telemetry.update();
         waitForStart();
 
-        while (opModeIsActive()) {
-            // Handle motor control for drive
-            double leftStickX = gamepad1.left_stick_x;
-            double leftStickY = gamepad1.left_stick_y;
-            double rightStickX = gamepad1.right_stick_x;
 
-            // Calculate and set wheel powers
-            double frontLeftPower = leftStickY + leftStickX;
-            double frontRightPower = leftStickY - leftStickX;
-            double backLeftPower = leftStickY + rightStickX;
-            double backRightPower = leftStickY - rightStickX;
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
 
-            motorFrontLeft.setPower(frontLeftPower);
-            motorFrontRight.setPower(frontRightPower);
-            motorBackLeft.setPower(backLeftPower);
-            motorBackRight.setPower(backRightPower);
-
-            // Handle arm position and control
-            if (gamepad1.right_bumper) {
-                armPosition = ARM_COLLECT;
-                wrist.setPosition(WRIST_FOLDED_OUT);
-                intake.setPosition(INTAKE_COLLECT);
-            } else if (gamepad1.left_bumper) {
-                armPosition = ARM_CLEAR_BARRIER;
-            } else if (gamepad1.y) {
-                armPosition = ARM_SCORE_SAMPLE_IN_LOW;
-            } else if (gamepad1.dpad_left) {
-                armPosition = ARM_COLLAPSED_INTO_ROBOT;
-                wrist.setPosition(WRIST_FOLDED_IN);
-                intake.setPosition(INTAKE_OFF);
-            } else if (gamepad1.dpad_right) {
-                armPosition = ARM_SCORE_SPECIMEN;
-                wrist.setPosition(WRIST_FOLDED_IN);
-            } else if (gamepad1.dpad_up) {
-                armPosition = ARM_ATTACH_HANGING_HOOK;
-                intake.setPosition(INTAKE_OFF);
-            } else if (gamepad1.dpad_down) {
-                armPosition = ARM_WINCH_ROBOT;
-                intake.setPosition(INTAKE_OFF);
+            // slew the servo, according to the rampUp (direction) variable.
+            if (rampUp) {
+                // Keep stepping up until we hit the max value.
+                position += INCREMENT ;
+//                if (position >= MAX_POS ) {
+//                    position = MAX_POS;
+//                    rampUp = !rampUp;   // Switch ramp direction
+//                }
+            }
+            else {
+                // Keep stepping down until we hit the min value.
+                position -= INCREMENT ;
+                if (position <= MIN_POS ) {
+                    position = MIN_POS;
+                    rampUp = !rampUp;  // Switch ramp direction
+                }
             }
 
-            // Fudge factor for arm control
-            armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.right_trigger + (-gamepad1.left_trigger));
-            armMotor.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
-            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            armMotor.setPower(0.1);
+            telemetry.addData("Servo Position", "%5.2f", position);
+            telemetry.addData(">", "Press Stop to end test." );
 
-            // Send telemetry
-            telemetry.addData("armTarget: ", armMotor.getTargetPosition());
-            telemetry.addData("arm Encoder: ", armMotor.getCurrentPosition());
+for (int j=0; j< SERVO_COUNT; ++j) {
+    // Display the current value
+
+
+    // Set the servo to the new position and pause;
+    servo[j].setPosition(position);
+
+    sleep(CYCLE_MS);
+    idle();
+
+    telemetry.addData("Servo  GetPosition", "%5.2f", servo[j].getPosition());
+
+
+}
             telemetry.update();
         }
+
+        // Signal done;
+        telemetry.addData(">", "Done");
+        telemetry.update();
     }
 }
