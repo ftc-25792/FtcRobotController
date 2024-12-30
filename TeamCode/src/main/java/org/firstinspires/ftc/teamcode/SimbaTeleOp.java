b0b1f2ff443fb54fc1366ae8f93095237feb4292
guptaempire1 <samsrishti2023@gmail.com>
2024-12-30 12:38:49 -0600

    Update SimbaTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimbaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimbaTeleOp.java
@@ -1,57 +1,30 @@
-package org.firstinspires.ftc.teamcode;package.org.firstinspires.ftc.teamcode;
-
-import com.google.blocks.ftcrobotcontroller.runtime.CRServoAccess;
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-
 @TeleOp(name = "SimbaTeleOp", group = "TeleOp")
 public class SimbaTeleOp extends LinearOpMode {
-
     private DcMotor leftFrontMotor;
     private DcMotor leftRearMotor;
     private DcMotor rightFrontMotor;
     private DcMotor rightRearMotor;
-    private DcMotor armMotor;
+    private DcMotor leftVertMotor;
+    private DcMotor rightVertMotor;
     private IMU imu;
-    CRServo intake;
-    Servo wrist;
-    double viperposition = 0.0;
-    final double ARM_TICKS_PER_DEGREE = 7125.16 / 360;
-    final double ARM_COLLAPSED_INTO_ROBOT = 0;
-    final double ARM_COLLECT = 10 * ARM_TICKS_PER_DEGREE;//250
-    final double ARM_SCORE_SAMPLE_IN_LOW = 30 * ARM_TICKS_PER_DEGREE; //160
-    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE; // Allow for slight adjustments
-    final double ARM_SCORE_IN_HIGH = 70 * ARM_TICKS_PER_DEGREE;
-
+    private Servo linear;
+    private Servo rightClaw;
+    private Servo leftClaw;
 
     //Viper Slide Control Variables
-    final double VIPERSLIDE_TICKS_PER_DEGREE = 2500 / 360;
-    final double VIPER_COLLECT = 5 * VIPERSLIDE_TICKS_PER_DEGREE;
-    //final double Maxposition = 1800 * VIPERSLIDE_TICKS_PER_DEGREE;
-    final double Maxposition = 26 * VIPERSLIDE_TICKS_PER_DEGREE;
+    final double VERTICAL_MOTION = 0.3;
 
-
-
-    double armPosition = ARM_COLLAPSED_INTO_ROBOT;
-    double armPositionFudgeFactor;
-    final double WRIST_FOLDED_IN = 0.8333;
-    final double WRIST_FOLDED_OUT = 0.5;
-    final double WRIST_FOLDED_MIN = 0.2;
-    final double ARM_CLEAR_BARRIER = 230 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SPECIMEN = 50 * ARM_TICKS_PER_DEGREE;//160
-    final double ARM_ATTACH_HANGING_HOOK = 90 * ARM_TICKS_PER_DEGREE;
-    final double ARM_WINCH_ROBOT = 0 * ARM_TICKS_PER_DEGREE;//15  //8
-    final double INTAKE_COLLECT = 1;
-    final double INTAKE_OFF = 0;
-    final double INTAKE_DEPOSIT = -1;
+    //Linear Servo Control
+    final double LINEAR_OUT = 1;
+    final double LINEAR_IN = 0;
 
     private static final double FORWARD_DISTANCE = 0.5; // Adjust distance in meters
     private static final double TURN_ANGLE = -90.0; // Degrees to turn
@@ -64,35 +37,64 @@ public class SimbaTeleOp extends LinearOpMode {
         rightFrontMotor = hardwareMap.get(DcMotor.class, "Right_front");
         leftRearMotor = hardwareMap.get(DcMotor.class, "Left_rear");
         rightRearMotor = hardwareMap.get(DcMotor.class, "Right_rear");
-        //armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        //intake = hardwareMap.get(CRServo.class, "intake");
-        //wrist = hardwareMap.get(Servo.class, "wrist");
-
+        leftVertMotor = hardwareMap.get(DcMotor.class, "Left_vert");
+        rightVertMotor = hardwareMap.get(DcMotor.class, "Right_vert");
+        linear = hardwareMap.get(Servo.class, "linear");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
         // Set motor directions
         rightRearMotor.setDirection(DcMotor.Direction.FORWARD);
         rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
         leftRearMotor.setDirection(DcMotor.Direction.REVERSE);
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
-
-//        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        ((DcMotorEx) armMotor).setCurrentAlert(2,CurrentUnit.AMPS);
+        leftVertMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightVertMotor.setDirection(DcMotor.Direction.FORWARD);
 
 //         Initialize the IMU
         imu = hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD; //BACK
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP; //BACK
         RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
         imu.initialize(new IMU.Parameters(orientationOnRobot));
-
         waitForStart();
+        double leftStickX = gamepad1.left_stick_x;
         double leftStickY = gamepad1.left_stick_y;
-        double rightStickY = gamepad1.right_stick_y;
+        double rightStickX = gamepad1.right_stick_x;
+        // Calculate wheel powers, incorporating strafing
+        double frontLeftPower = leftStickY + leftStickX - rightStickX;
+        double frontRightPower = leftStickY - leftStickX + rightStickX;
+        double backLeftPower = leftStickY + rightStickX + leftStickX;
+        double backRightPower = leftStickY - rightStickX - leftStickX;
+        // Set motor powers
+        leftFrontMotor.setPower(frontLeftPower);
+        rightFrontMotor.setPower(frontRightPower);
+        leftRearMotor.setPower(backLeftPower);
+        rightRearMotor.setPower(backRightPower);
 
-        // Set motor powers (adjust as needed for your drivetrain configuration)
+        if (gamepad1.dpad_down) {
+            linear.setPosition(LINEAR_IN);
+        } else if (gamepad1.dpad_up) {
+            linear.setPosition(LINEAR_OUT);
+        }
 
-        leftFrontMotor.setPower(leftStickY + rightStickY);
-        leftRearMotor.setPower(leftStickY + rightStickY);
-        rightFrontMotor.setPower(rightStickY - leftStickY);
-        rightRearMotor.setPower(rightStickY - leftStickY);
+        //moves viper slides a small amount ("tick")
+        if (gamepad1.right_bumper) {
+            rightVertMotor.setPower(VERTICAL_MOTION);
+            leftVertMotor.setPower(VERTICAL_MOTION);
+        } else if (gamepad1.left_bumper) {
+            rightVertMotor.setPower(-VERTICAL_MOTION);
+            leftVertMotor.setPower(-VERTICAL_MOTION);
+        } else if (gamepad1.a) {
+            rightVertMotor.setPower(0.0);
+        }
+
+        if (gamepad1.x) {
+            rightClaw.setPosition(1.0);
+            leftClaw.setPosition(-1.0);
+        } else if (gamepad1.y) {
+            rightClaw.setPosition(-1.0);
+            leftClaw.setPosition(1.0);
+        }
+
+    }
+}
