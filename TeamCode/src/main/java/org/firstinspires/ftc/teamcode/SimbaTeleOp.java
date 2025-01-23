5d82763fe2dabbfd30a5e42a1523def04745ff08
guptaempire1 <samsrishti2023@gmail.com>
2025-01-22 18:22:14 -0600

    Updated SimbaTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimbaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimbaTeleOp.java
@@ -1,35 +1,23 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "SimbaTeleOp", group = "TeleOp")
+@TeleOp(name="SimbaTeleOp1/8.3", group="TeleOp")
 public class SimbaTeleOp extends LinearOpMode {
+
     private DcMotor leftFrontMotor;
     private DcMotor leftRearMotor;
     private DcMotor rightFrontMotor;
     private DcMotor rightRearMotor;
     private DcMotor leftVertMotor;
     private DcMotor rightVertMotor;
-    private IMU imu;
-    private Servo linear;
+    private Servo linearServo;
     private Servo rightClaw;
-    private Servo leftClaw;
-
-    //Viper Slide Control Variables
-    final double VERTICAL_MOTION = 0.3;
-
-    //Linear Servo Control
-    final double LINEAR_OUT = 1;
-    final double LINEAR_IN = 0;
-
-    private static final double FORWARD_DISTANCE = 0.5; // Adjust distance in meters
-    private static final double TURN_ANGLE = -90.0; // Degrees to turn
-    private static final double DRIVE_SPEED = 0.9; // Speed for driving
-
+    final double viperPower = 1.0;
     @Override
     public void runOpMode() {
         // Initialize motors
@@ -37,64 +25,77 @@ public class SimbaTeleOp extends LinearOpMode {
         rightFrontMotor = hardwareMap.get(DcMotor.class, "Right_front");
         leftRearMotor = hardwareMap.get(DcMotor.class, "Left_rear");
         rightRearMotor = hardwareMap.get(DcMotor.class, "Right_rear");
+
         leftVertMotor = hardwareMap.get(DcMotor.class, "Left_vert");
         rightVertMotor = hardwareMap.get(DcMotor.class, "Right_vert");
-        linear = hardwareMap.get(Servo.class, "linear");
+
+        linearServo = hardwareMap.get(Servo.class, "linearServo");
         rightClaw = hardwareMap.get(Servo.class, "rightClaw");
-        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
-        // Set motor directions
+        rightVertMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightRearMotor.setDirection(DcMotor.Direction.FORWARD);
         rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
         leftRearMotor.setDirection(DcMotor.Direction.REVERSE);
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
-        leftVertMotor.setDirection(DcMotor.Direction.FORWARD);
-        rightVertMotor.setDirection(DcMotor.Direction.FORWARD);
 
-//         Initialize the IMU
-        imu = hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
-        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP; //BACK
-        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
-        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
         waitForStart();
-        double leftStickX = gamepad1.left_stick_x;
-        double leftStickY = gamepad1.left_stick_y;
-        double rightStickX = gamepad1.right_stick_x;
-        // Calculate wheel powers, incorporating strafing
-        double frontLeftPower = leftStickY + leftStickX - rightStickX;
-        double frontRightPower = leftStickY - leftStickX + rightStickX;
-        double backLeftPower = leftStickY + rightStickX + leftStickX;
-        double backRightPower = leftStickY - rightStickX - leftStickX;
-        // Set motor powers
-        leftFrontMotor.setPower(frontLeftPower);
-        rightFrontMotor.setPower(frontRightPower);
-        leftRearMotor.setPower(backLeftPower);
-        rightRearMotor.setPower(backRightPower);
 
-        if (gamepad1.dpad_down) {
-            linear.setPosition(LINEAR_IN);
-        } else if (gamepad1.dpad_up) {
-            linear.setPosition(LINEAR_OUT);
+        while (opModeIsActive()) {
+            // Drive controls
+            // Handle motor control
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double leftFrontPower = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
+
+            // Set motor powers
+            leftFrontMotor.setPower(leftFrontPower);
+            rightFrontMotor.setPower(rightFrontPower);
+            leftRearMotor.setPower(leftBackPower);
+            rightRearMotor.setPower(rightBackPower);
+            if (gamepad1.right_bumper) {
+                leftVertMotor.setPower(viperPower);
+                rightVertMotor.setPower(viperPower);
+            }
+            else if(gamepad1.dpad_down){
+                leftVertMotor.setPower(0);
+                rightVertMotor.setPower(0);
+            }
+            else if (gamepad1.left_bumper) {
+                leftVertMotor.setPower(-viperPower);
+                rightVertMotor.setPower(-viperPower);
+            }
+            else if (gamepad1.right_stick_button){
+                leftVertMotor.setPower(0);
+                rightVertMotor.setPower(0);
+            }
+
+            // Linear servo control
+            if (gamepad1.a) {
+                linearServo.setPosition(1.0); // Fully extended
+            } else if (gamepad1.b) {
+                linearServo.setPosition(0.0); // Fully retracted
+            }
+
+            // Claw control - synced servos
+            if (gamepad1.x) {
+                rightClaw.setPosition(-1.0);
+                updateTelemetry("Claw Postion:",rightClaw.getPosition());
+              } else if (gamepad1.y) {
+                rightClaw.setPosition(0.5);
+                updateTelemetry("Claw Postion:",rightClaw.getPosition());
+            }
         }
-
-        //moves viper slides a small amount ("tick")
-        if (gamepad1.right_bumper) {
-            rightVertMotor.setPower(VERTICAL_MOTION);
-            leftVertMotor.setPower(VERTICAL_MOTION);
-        } else if (gamepad1.left_bumper) {
-            rightVertMotor.setPower(-VERTICAL_MOTION);
-            leftVertMotor.setPower(-VERTICAL_MOTION);
-        } else if (gamepad1.a) {
-            rightVertMotor.setPower(0.0);
-        }
-
-        if (gamepad1.x) {
-            rightClaw.setPosition(1.0);
-            leftClaw.setPosition(-1.0);
-        } else if (gamepad1.y) {
-            rightClaw.setPosition(-1.0);
-            leftClaw.setPosition(1.0);
-        }
-
+    }
+    public void updateTelemetry(String caption, double data)
+    {
+        telemetry.addData(caption, data);
+        telemetry.update();
     }
 }
