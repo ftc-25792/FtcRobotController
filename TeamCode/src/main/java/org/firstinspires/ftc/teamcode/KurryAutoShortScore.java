a76bbdbcfabe4070802e7c1a720d08fe57fc6109
FlashDragonCoderX-KB <balasubramaniamkrithik@gmail.com>
2026-01-03 14:09:15 -0600

    Update KurryAutoShortScore.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KurryAutoShortScore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KurryAutoShortScore.java
@@ -3,272 +3,277 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import java.util.List;
 
-@Autonomous(name="KurryAutoShortScore", group="Auto")
+@Autonomous(name = "--KurryAutoShootTT", group = "Linear Opmode")
 public class KurryAutoShortScore extends LinearOpMode {
 
-    DcMotor fl, fr, bl, br;
-    DcMotor intake, launcherL, launcherR;
-    Servo flapperL, flapperR;
-    CRServo divider;
-    IMU imu;
+    private DcMotor frontLeft, frontRight, backLeft, backRight;
+    private DcMotor launcherLeft, launcherRight, intake;
+    private IMU imu;
+    private Servo flapperRight, flapperLeft;
+    private CRServo divider;
 
+    private ElapsedTime runtime = new ElapsedTime();
+    private AprilTagHelper aprilTagHelper;
+    private String pattern = "UNKNOWN";
 
-    BallOrderDetector detector;
-    String order = "UNKNOWN";
+    private static final double DRIVE_SPEED = 0.5;
+    private static final double TURN_SPEED = 0.5;
+    private static final double HEADING_THRESHOLD = 1.0;
+    private static final double TICKS_PER_INCH = 537.7 / (Math.PI * 4); // 4" wheels, 537.7 ticks/rev
 
-
-    static final double DRIVE_SPEED = 0.5;
-    static final double TURN_KP = 0.012;
-    static final double TURN_MIN = 0.08;
-    static final double HEADING_TOLERANCE = 1.0;
-
-    static final double COUNTS_PER_REV = 537.7;
-    static final double WHEEL_DIAMETER = 4.0;
-    static final double COUNTS_PER_INCH =
-            COUNTS_PER_REV / (Math.PI * WHEEL_DIAMETER);
-
+    // Telemetry variables
+    private double targetHeading = 0;
+    private double driveSpeed = 0;
+    private double turnSpeed = 0;
+    private double leftSpeed = 0;
+    private double rightSpeed = 0;
+    private double headingError = 0;
 
     @Override
     public void runOpMode() {
 
-        initHardware();
-        initIMU();
+        // ===== Initialize Motors & Servos =====
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-        detector = new BallOrderDetector(hardwareMap);
+        launcherLeft = hardwareMap.get(DcMotor.class, "launcherRight"); // swapped intentionally
+        launcherRight = hardwareMap.get(DcMotor.class, "launcherLeft");
+        launcherLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        launcherRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        while (!isStarted() && !isStopRequested()) {
-            order = detector.getBallOrder();
-            telemetry.addData("Detected Order", order);
-            telemetry.update();
-        }
+        intake = hardwareMap.get(DcMotor.class, "intake");
 
-        detector.stop();
-        waitForStart();
+        flapperRight = hardwareMap.get(Servo.class, "fr");
+        flapperLeft = hardwareMap.get(Servo.class, "fl");
+        flapperRight.setDirection(Servo.Direction.FORWARD);
+        flapperLeft.setDirection(Servo.Direction.REVERSE);
 
-        driveStraight(-18);
-        turnTo(0);
+        divider = hardwareMap.get(CRServo.class, "sw");
 
-        runIntakeSort(order);
-        launchSequence();
-
-
-    }
-
-    void initHardware() {
-        fl = hardwareMap.get(DcMotor.class,"frontLeft");
-        fr = hardwareMap.get(DcMotor.class,"frontRight");
-        bl = hardwareMap.get(DcMotor.class,"backLeft");
-        br = hardwareMap.get(DcMotor.class,"backRight");
-
-        intake = hardwareMap.get(DcMotor.class,"intake");
-        launcherL = hardwareMap.get(DcMotor.class,"launcherLeft");
-        launcherR = hardwareMap.get(DcMotor.class,"launcherRight");
-
-        flapperL = hardwareMap.get(Servo.class,"fl");
-        flapperR = hardwareMap.get(Servo.class,"fr");
-        divider = hardwareMap.get(CRServo.class,"sw");
-
-        fl.setDirection(DcMotor.Direction.REVERSE);
-        bl.setDirection(DcMotor.Direction.REVERSE);
-        launcherL.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        flapperL.setDirection(Servo.Direction.REVERSE);
-        flapperR.setPosition(0.71);
-        flapperL.setPosition(0.3);
-    }
-
-    void initIMU() {
-        imu = hardwareMap.get(IMU.class,"imu");
-        imu.initialize(new IMU.Parameters(
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters imuParams = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
                         RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD)));
+                        RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
+                )
+        );
+        imu.initialize(imuParams);
         imu.resetYaw();
-    }
 
-
-    void driveStraight(double inches) {
-        int move = (int)(inches * COUNTS_PER_INCH);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
 
         resetEncoders();
-        fl.setTargetPosition(move);
-        fr.setTargetPosition(move);
-        bl.setTargetPosition(move);
-        br.setTargetPosition(move);
 
-        setRunToPosition(DRIVE_SPEED);
-
-        while (opModeIsActive() &&
-                (fl.isBusy() || fr.isBusy())) {}
-
-        stopDrive();
-    }
+        waitForStart();
+        aprilTagHelper = new AprilTagHelper(hardwareMap);
+        telemetry.addLine("AprilTag Helper initialized...");
+        telemetry.update();
+        sleep(500);
 
 
-    void turnTo(double targetAngle) {
 
-        while (opModeIsActive()) {
+        List detections = aprilTagHelper.getDetections();
+        while (detections.isEmpty()) {
+            aprilTagHelper.telemetryAprilTag(telemetry);
+            telemetry.update();
+            detections = aprilTagHelper.getDetections();
+           // sleep(1000);
 
-            double current = getHeading();
-            double error = AngleUnit.normalizeDegrees(targetAngle - current);
+        }
+        telemetry.addLine("outside while");
+        telemetry.update();
 
-            if (Math.abs(error) <= HEADING_TOLERANCE) break;
+        {
+            int id = ((org.firstinspires.ftc.vision.apriltag.AprilTagDetection) detections.get(0)).id;
+            switch (id) {
+                case 21: pattern = "GPP"; break;
+                case 22: pattern = "PGP"; break;
+                case 23: pattern = "PPG"; break;
+            }
+            telemetry.addData("Pattern detected", pattern);
 
-            double turnPower = Range.clip(error * TURN_KP, -0.4, 0.4);
-
-            if (Math.abs(turnPower) < TURN_MIN)
-                turnPower = TURN_MIN * Math.signum(turnPower);
-
-            fl.setPower(turnPower);
-            bl.setPower(turnPower);
-            fr.setPower(-turnPower);
-            br.setPower(-turnPower);
+            telemetry.update();
+            sleep(1000);
         }
 
-        stopDrive();
+
+       // waitForStart();
+
+        launcherLeft.setPower(0.5);
+        launcherRight.setPower(0.5);
+        sleep(1000);
+
+//        driveStraight(2, true);        // move into shooting position
+        launch(pattern);               // shoot according to pattern
+//        driveStraight(8, true);        // continue autonomous
+
+        // Stop camera
+        aprilTagHelper.stop();
+    }
+
+    private void launch(String pattern) {
+        divider.setPower(1);
+        intake.setPower(0.8);
+        sleep(500);
+
+        switch (pattern) {
+            case "PPG":
+                rightLaunch();
+                divide();
+                rightLaunch();
+                leftLaunch();
+
+                break;
+            case "PGP":
+//                sleep(1000);
+                rightLaunch();
+                leftLaunch();
+                divide();
+                rightLaunch();
+                break;
+            case "GPP":
+//                sleep(1000);
+                leftLaunch();
+                rightLaunch();
+                divide();
+                rightLaunch();
+                break;
+            default: // fallback
+                leftLaunch();
+                rightLaunch();
+                break;
+        }
+
+        divider.setPower(0);
+        intake.setPower(0);
+        launcherLeft.setPower(0);
+        launcherRight.setPower(0);
+    }
+    private void divide(){
+        sleep(500);
+        divider.setPower(1);//move to the right
+        sleep(1000);
+    }
+
+    private void rightLaunch() {
+        flapperRight.setPosition(0.58);
+        sleep(1000);
+        flapperRight.setPosition(0.71);
+        sleep(1000);
+    }
+
+    private void leftLaunch() {
+        flapperLeft.setPosition(0.14);
+        sleep(1000);
+        flapperLeft.setPosition(0.3);
+        sleep(1000);
+    }
+
+    private void resetEncoders() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void setAllPower(double p) {
+        frontLeft.setPower(p);
+        frontRight.setPower(p);
+        backLeft.setPower(p);
+        backRight.setPower(p);
+    }
+
+    private void stopAll() {
+        setAllPower(0);
+    }
+
+    private void driveStraight(double inches, boolean forward) {
+        int move = (int) (inches * TICKS_PER_INCH);
+        if (!forward) move = -move;
+
+        resetEncoders();
+        frontLeft.setTargetPosition(move);
+        frontRight.setTargetPosition(move);
+        backLeft.setTargetPosition(move);
+        backRight.setTargetPosition(move);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        setAllPower(DRIVE_SPEED);
+        runtime.reset();
+        while (opModeIsActive() &&
+                (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy()) &&
+                runtime.seconds() < 5) {
+            telemetry.addData("Driving", inches);
+            telemetry.update();
+        }
+        stopAll();
         sleep(100);
     }
 
-    double getHeading() {
-        YawPitchRollAngles ypr = imu.getRobotYawPitchRollAngles();
-        return ypr.getYaw(AngleUnit.DEGREES);
-    }
-
-
-    void runIntakeSort(String order) {
-
-        intake.setPower(0.8);
-
-        switch (order) {
-            case "PGP":
-                pulseDivider(true);
-                pulseDivider(false);
-                break;
-
-            case "PPG":
-                pulseDivider(false);
-                pulseDivider(false);
-                break;
-
-            case "GPP":
-                pulseDivider(true);
-                pulseDivider(true);
-                break;
-
-            default:
-                pulseDivider(true);
-                pulseDivider(false);
-        }
-
-        intake.setPower(0);
-    }
-
-    void pulseDivider(boolean right) {
-        divider.setPower(right ? 1 : -1);
-        sleep(450);
-        divider.setPower(0);
-        sleep(120);
-    }
-
-
-    void launchSequence() {
-
-        launcherL.setPower(0.42);
-        launcherR.setPower(0.52);
-        sleep(2000);
-
-        rightLaunch();
-        leftLaunch();
-
-        launcherL.setPower(0);
-        launcherR.setPower(0);
-    }
-
-    void rightLaunch() {
-        flapperR.setPosition(0.58);
-        sleep(700);
-        flapperR.setPosition(0.71);
-        sleep(700);
-    }
-
-    void leftLaunch() {
-        flapperL.setPosition(0.14);
-        sleep(700);
-        flapperL.setPosition(0.3);
-        sleep(700);
-    }
-
-
-    
-    void resetEncoders() {
-        for (DcMotor m : new DcMotor[]{fl,fr,bl,br}) {
-            m.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            m.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-    }
-
-    void setRunToPosition(double pwr) {
-        for (DcMotor m : new DcMotor[]{fl,fr,bl,br}) {
-            m.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            m.setPower(pwr);
-        }
-    }
-
-    void strafing(double inches, boolean isLeft) {
+    private void strafing(double inches, boolean left) {
         double correction = 1.12;
-        int move = (int)(inches * COUNTS_PER_INCH * correction);
+        int move = (int) (inches * TICKS_PER_INCH * correction);
 
         resetEncoders();
 
-        if (isLeft) {
-            fl.setTargetPosition(-move);
-            fr.setTargetPosition(move);
-            bl.setTargetPosition(move);
-            br.setTargetPosition(-move);
+        if (left) {
+            frontLeft.setTargetPosition(-move);
+            frontRight.setTargetPosition(move);
+            backLeft.setTargetPosition(move);
+            backRight.setTargetPosition(-move);
         } else {
-            fl.setTargetPosition(move);
-            fr.setTargetPosition(-move);
-            bl.setTargetPosition(-move);
-            br.setTargetPosition(move);
+            frontLeft.setTargetPosition(move);
+            frontRight.setTargetPosition(-move);
+            backLeft.setTargetPosition(-move);
+            backRight.setTargetPosition(move);
         }
 
-        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         setAllPower(DRIVE_SPEED);
-
-        ElapsedTime runtime = new ElapsedTime();
         runtime.reset();
         while (opModeIsActive() &&
-                (fl.isBusy() || fr.isBusy() || bl.isBusy() || br.isBusy()) &&
+                (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy()) &&
                 runtime.seconds() < 5) {
             telemetry.addData("Strafing", inches);
             telemetry.update();
         }
-
-        stopDrive();
+        stopAll();
         sleep(100);
     }
 
-    void setAllPower(double power) {
-        fl.setPower(power);
-        fr.setPower(power);
-        bl.setPower(power);
-        br.setPower(power);
-    }
 
-
-    void stopDrive() {
-        for (DcMotor m : new DcMotor[]{fl,fr,bl,br})
-            m.setPower(0);
+    public double getHeading() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        return orientation.getYaw(AngleUnit.DEGREES);
     }
 }
