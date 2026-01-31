190e15b9514704c52a3a95480862820d2d1a297e
Krithik Balasubramaniam <balasubramaniamkrithik@gmail.com>
2026-01-31 10:46:11 -0600

    This has the added camera alignment in teleOP. And a basic PID control for gobilda 6000 rpm motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KurryTeleOpFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KurryTeleOpFinal.java
@@ -1,183 +1,235 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name = "--KURRY TeleOp", group = "Linear Opmode")
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+import java.util.List;
+
+@TeleOp(name = "--KURRYTeleOpV.M&K", group = "Linear Opmode")
 public class KurryTeleOpFinal extends LinearOpMode {
+    // Hardware Members
     private DcMotor frontLeft, frontRight, backLeft, backRight, intake;
     private DcMotorEx launcherLeft, launcherRight;
     private Servo flapperLeft, flapperRight;
     private CRServo servoWheel;
+    private IMU imu;
 
-    private boolean isHoldingTriggerR = false;
-    private boolean isHoldingPositionR = false;
-    private boolean isHoldingTriggerL = false;
-    private boolean isHoldingPositionL = false;
-    private int holdPosition = 0;
+    // State Machine
+    private KurryStateDrive CurrentState = KurryStateDrive.eDrive;
+    private double diff = 4;
 
-    // Servo constants
-    final double ServoWheelRIGHT = 1;
-    final double ServoWheelSTOP = 0;
-    final double ServoWheelLEFT = -1;
-
-    // Launcher speed presets
-    private double leftLaunchPower = 0.4;
-    private double rightLaunchPower = 0.4;
-    private double leftLauncherPowerMID = 0.48;
-    private double rightLauncherPowerMID = 0.55;
-    private double leftLauncherXC = 0.75;
-    private double rightLauncherXC = 0.75;
-
-
-
+    // Constants - Driving
     private static final double SPEED_FACTOR = 0.7;
+    private Alliance alliance = Alliance.eRed;
+    private static double SIGN_Alliance = -1;
+
+    // Constants - AprilTag Alignment
+    private static final double SPEED_GAIN = 0.03;
+    private static final double TURN_GAIN  = 0.01;
+    private static final double STRAFE_GAIN = 0.02;
+    private static final double MAX_AUTO_SPEED = 0.5;
+    private static final double MAX_AUTO_TURN  = 0.3;
+    private static final double MAX_AUTO_STRAFE = 0.5;
+
+    // Tolerance for Auto-Exit
+    private static final double RANGE_TOLERANCE = 1.0;   // inches
+    private static final double BEARING_TOLERANCE = 2.0; // degrees
+    private static final double YAW_TOLERANCE = 2.0;     // degrees
+
+    private AprilTagHelper aprilTagHelper;
+
+    // State enums
+    enum KurryStateDrive { eDrive, eAlign_POST }
+    enum Alliance { eRed, eBlue }
 
     @Override
     public void runOpMode() {
-        // Hardware mapping
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        // --- Hardware Map ---
+        frontLeft   = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft    = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight   = hardwareMap.get(DcMotor.class, "backRight");
         launcherRight = hardwareMap.get(DcMotorEx.class, "launcherRight");
-        launcherLeft = hardwareMap.get(DcMotorEx.class, "launcherLeft");
-
-        intake = hardwareMap.get(DcMotor.class, "intake");
+        launcherLeft  = hardwareMap.get(DcMotorEx.class, "launcherLeft");
+        intake      = hardwareMap.get(DcMotor.class, "intake");
         flapperLeft = hardwareMap.get(Servo.class, "fl");
         flapperRight = hardwareMap.get(Servo.class, "fr");
-        servoWheel = hardwareMap.get(CRServo.class, "sw");
+        servoWheel  = hardwareMap.get(CRServo.class, "sw");
+        imu = hardwareMap.get(IMU.class, "imu");
 
-        // Motor directions
+
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
-
-
         backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.FORWARD);
-
 
         launcherLeft.setDirection(DcMotor.Direction.REVERSE);
         launcherRight.setDirection(DcMotor.Direction.FORWARD);
-
-        // Servo directions
         flapperLeft.setDirection(Servo.Direction.REVERSE);
         flapperRight.setDirection(Servo.Direction.FORWARD);
 
-        // Zero power behaviors
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        telemetry.addLine("KURRY TeleOp Initialized");
+
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD)));
+
+
+        aprilTagHelper = new AprilTagHelper(hardwareMap);
+
+
+        telemetry.addLine("Ready to Start");
         telemetry.update();
+        while (!isStarted() && !isStopRequested()) {
+
+            if (gamepad1.b){
+                alliance = Alliance.eRed;
+            }
+            if (gamepad1.x){
+                alliance = Alliance.eBlue;
+            }
+
+            SIGN_Alliance = (alliance == Alliance.eBlue) ? 1 : -1;
+
+            telemetry.addData("Alliance", alliance);
+            telemetry.addData("Sign", SIGN_Alliance);
+            telemetry.update();
+        }
 
         waitForStart();
 
+
         while (opModeIsActive()) {
-            // Telemetry
-            telemetry.addData("Left Servo Pos", "%.2f", flapperLeft.getPosition());
-            telemetry.addData("Right Servo Pos", "%.2f", flapperRight.getPosition());
+            switch (CurrentState) {
+                case eDrive:
+                    driveControls();
+                    if (gamepad1.a) CurrentState = KurryStateDrive.eAlign_POST;
+                    break;
 
-            // Drive control (basic tank/mecanum hybrid)
-            double leftStickX = gamepad1.left_stick_x * SPEED_FACTOR;
-            double leftStickY = -gamepad1.left_stick_y * SPEED_FACTOR;
-            double rightStickX = gamepad1.right_stick_x * SPEED_FACTOR;
-
-            double frontLeftPower = leftStickY + leftStickX;
-            double frontRightPower = leftStickY - leftStickX;
-            double backLeftPower = leftStickY + rightStickX;
-            double backRightPower = leftStickY - rightStickX;
-
-            frontLeft.setPower(frontLeftPower);
-            frontRight.setPower(frontRightPower);
-            backLeft.setPower(backLeftPower);
-            backRight.setPower(backRightPower);
-
-            // Launcher control
-            double powerR = 0;
-            double powerL = 0;
-
-            if (gamepad2.right_trigger> 0.2) powerR = 0.467;
-            if (gamepad2.left_trigger>0.2) powerL = 0.43;
-            if (gamepad2.left_bumper) powerL = leftLauncherPowerMID;
-            if (gamepad2.right_bumper) powerR = rightLauncherPowerMID;
-            if (gamepad2.dpad_up) powerL = leftLauncherXC;
-            if (gamepad2.y) powerR = rightLauncherXC;
-
-            launcherRight.setPower(powerR);
-            launcherLeft.setPower(powerL);
-
-            // Intake control
-            if (gamepad1.right_trigger > 0.2) {
-                isHoldingTriggerR = true;
-                isHoldingPositionR = false;
-                intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                intake.setPower(0.8);
-            } else if (isHoldingTriggerR) {
-                isHoldingTriggerR = false;
-                isHoldingPositionR = true;
-                holdPosition = intake.getCurrentPosition();
-                intake.setTargetPosition(holdPosition);
-                intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                intake.setPower(0.3);
+                case eAlign_POST:
+                    alignPostTeleOp();
+                    if (gamepad1.b) { stopAll(); CurrentState = KurryStateDrive.eDrive; }
+                    break;
             }
-
-            if (gamepad1.left_trigger > 0.2) {
-                isHoldingTriggerL = true;
-                isHoldingPositionL = false;
-                intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                intake.setPower(-0.8);
-            } else if (isHoldingTriggerL) {
-                isHoldingTriggerL = false;
-                isHoldingPositionL = true;
-                holdPosition = intake.getCurrentPosition();
-                intake.setTargetPosition(holdPosition);
-                intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                intake.setPower(0.8);
-            }
-
-            // Flapper control (no sleep, just toggle positions)
-            if (gamepad2.b) {
-                flapperLeft.setPosition(0.4);
-            } else {
-                flapperLeft.setPosition(0.55);
-            }
-
-            if (gamepad2.dpad_left) {
-                flapperRight.setPosition(0.65);
-            } else {
-                flapperRight.setPosition(0.82);
-            }
-
-            // Servo wheel control
-            if (gamepad2.left_stick_button) {
-                servoWheel.setPower(ServoWheelRIGHT);
-            } else if (gamepad2.right_stick_button) {
-                servoWheel.setPower(ServoWheelLEFT);
-            } else if (gamepad2.dpad_down) {
-                servoWheel.setPower(ServoWheelSTOP);
-            }
-
-            // Telemetry
-            telemetry.addData("Left Launch Power", "%.2f", powerL);
-            telemetry.addData("Right Launch Power", "%.2f", powerR);
-            telemetry.addData("Flapper left Pos", "%.2f", flapperLeft.getPosition());
-            telemetry.addData("Flapper right Pos", "%.2f", flapperRight.getPosition());
             telemetry.update();
-
-            sleep(50);
         }
+    }
+
+    private void driveControls() {
+
+        double y  = -gamepad1.left_stick_y;
+        double x  =  gamepad1.left_stick_x;
+        double rx =  gamepad1.right_stick_x;
+        moveRobot(y * SPEED_FACTOR, x * SPEED_FACTOR, rx * SPEED_FACTOR);
+
+
+        double pL = 0, pR = 0;
+        if (gamepad2.left_trigger > 0.2) pL = 0.43;
+        else if (gamepad2.left_bumper)  pL = 0.48;
+        else if (gamepad2.dpad_up)      pL = 0.75;
+
+        if (gamepad2.right_trigger > 0.2){
+            pR = 0.467;
+        }
+        else if (gamepad2.right_bumper) {
+            pR = 0.55;
+        }
+        else if (gamepad2.y){
+            pR = 0.75;
+        }
+
+        launcherLeft.setPower(pL);
+        launcherRight.setPower(pR);
+
+        if (gamepad1.right_trigger > 0.2) {
+            intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            intake.setPower(0.8);
+        } else if (gamepad1.left_trigger > 0.2) {
+            intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            intake.setPower(-0.8);
+        } else {
+            if (intake.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
+                intake.setTargetPosition(intake.getCurrentPosition());
+                intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                intake.setPower(0.5);
+            }
+        }
+
+
+        flapperLeft.setPosition(gamepad2.b ? 0.4 : 0.55);
+        flapperRight.setPosition(gamepad2.dpad_left ? 0.65 : 0.82);
+
+        if (gamepad2.left_stick_button)       servoWheel.setPower(1.0);
+        else if (gamepad2.right_stick_button) servoWheel.setPower(-1.0);
+        else if (gamepad2.dpad_down)          servoWheel.setPower(0);
+    }
+
+    private void alignPostTeleOp() {
+        List<AprilTagDetection> detections = aprilTagHelper.getDetections();
+
+        if (detections != null && !detections.isEmpty()) {
+            AprilTagDetection tag = detections.get(0);
+
+            double rangeError   = tag.ftcPose.range - 40;
+            double bearingError = tag.ftcPose.bearing + (diff*SIGN_Alliance);
+            double yawError     = tag.ftcPose.yaw;
+
+            boolean isDistanceAligned = Math.abs(rangeError) < RANGE_TOLERANCE;
+            boolean isBearingAligned  = Math.abs(bearingError) < BEARING_TOLERANCE;
+            boolean isYawAligned      = Math.abs(yawError) < YAW_TOLERANCE;
+
+            if (isDistanceAligned && isBearingAligned && isYawAligned) {
+                stopAll();
+                gamepad1.rumble(250);
+                CurrentState = KurryStateDrive.eDrive;
+                return;
+            }
+
+            double drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+            double turn   = Range.clip(bearingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+            double strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+
+            moveRobot(drive, strafe, turn);
+
+            telemetry.addData("AutoAlign", "Active");
+            telemetry.addData("Errors (R/B/Y)", "%5.1f / %5.1f / %5.1f", rangeError, bearingError, yawError);
+        } else {
+            telemetry.addLine("No POST detected - Searching...");
+            stopAll();
+        }
+    }
+
+    private void moveRobot(double y, double x, double rx) {
+        double fl = y + x + rx;
+        double fr = y - x - rx;
+        double bl = y - x + rx;
+        double br = y + x - rx;
+
+        double max = Math.max(Math.abs(fl), Math.max(Math.abs(fr), Math.max(Math.abs(bl), Math.abs(br))));
+        if (max > 1.0) { fl /= max; fr /= max; bl /= max; br /= max; }
+
+        frontLeft.setPower(fl);
+        frontRight.setPower(fr);
+        backLeft.setPower(bl);
+        backRight.setPower(br);
+    }
+
+    private void stopAll() {
+        frontLeft.setPower(0); frontRight.setPower(0);
+        backLeft.setPower(0); backRight.setPower(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDLF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDLF.java
@@ -3,85 +3,45 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
-@Autonomous(name="Drake<Kendrick", group="Autonomous")
-public class CertifiedLoverBoy extends LinearOpMode {
+@Autonomous(name = "One Motor Velocity Test", group = "Auto")
+public class PIDLF extends LinearOpMode {
 
-    private DcMotorEx launcherLeft, launcherRight;
-
-    // Motor / encoder specs
-    private static final int ENCODER_TICKS_PER_REV = 28; // RS-555 output shaft
-    private static final double MAX_RPM = 6000;
-    private static final double MAX_TPS = MAX_RPM * ENCODER_TICKS_PER_REV / 60.0;
-
-    private static final double TARGET_SPEED_PERCENT = 0.7; // 70% of max speed
-    private static final double TARGET_TPS = MAX_TPS * TARGET_SPEED_PERCENT;
-
-    private static final double Kp = 0.0005;
-    private static final double Ki = 0.000001;
-    private static final double Kd = 0.0001;
+    DcMotorEx motor;
 
     @Override
     public void runOpMode() {
 
-        // Hardware mapping
-        launcherLeft = hardwareMap.get(DcMotorEx.class, "launcherLeft");
-        launcherRight = hardwareMap.get(DcMotorEx.class, "launcherRight");
+        motor = hardwareMap.get(DcMotorEx.class, "motor");
 
-        // Motor directions
-        launcherLeft.setDirection(DcMotorEx.Direction.REVERSE);
-        launcherRight.setDirection(DcMotorEx.Direction.FORWARD);
+        motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
-        telemetry.addLine("Launcher PID Initialized");
-        telemetry.addData("Target TPS (70%)", TARGET_TPS);
+        motor.setVelocityPIDFCoefficients(
+                0.012,
+                0.0,
+                0.0004,
+                14.0
+        );
+
+        telemetry.addLine("Ready");
         telemetry.update();
 
         waitForStart();
 
-        double integralL = 0, integralR = 0;
-        double lastErrorL = 0, lastErrorR = 0;
-        ElapsedTime timer = new ElapsedTime();
-
-        while (opModeIsActive()) {
-
-            double dt = timer.seconds();
-            timer.reset();
-
-            double velL = launcherLeft.getVelocity();
-            double velR = launcherRight.getVelocity();
-
-            double errorL = TARGET_TPS - velL;
-            integralL += errorL * dt;
-            double derivativeL = (errorL - lastErrorL) / dt;
-            double powerL = Kp * errorL + Ki * integralL + Kd * derivativeL;
-            powerL = Range.clip(powerL, 0, 1);
-            lastErrorL = errorL;
-
-            double errorR = TARGET_TPS - velR;
-            integralR += errorR * dt;
-            double derivativeR = (errorR - lastErrorR) / dt;
-            double powerR = Kp * errorR + Ki * integralR + Kd * derivativeR;
-            powerR = Range.clip(powerR, 0, 1);
-            lastErrorR = errorR;
+        if (opModeIsActive()) {
 
 
-            launcherLeft.setPower(powerL);
-            launcherRight.setPower(powerR);
+            double targetVelocity = 2240;
+
+            motor.setVelocity(targetVelocity);
 
 
-            telemetry.addData("Launcher Left TPS", "%.1f TPS", velL);
-            telemetry.addData("Launcher Right TPS", "%.1f TPS", velR);
-            telemetry.addData("Power Left", "%.2f", powerL);
-            telemetry.addData("Power Right", "%.2f", powerR);
-            telemetry.update();
+            sleep(2000);
 
-            sleep(20);
+
+            motor.setVelocity(0);
         }
-
-
-        launcherLeft.setPower(0);
-        launcherRight.setPower(0);
     }
 }
